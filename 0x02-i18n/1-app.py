ea53ce2a217c2defd316d44e6dd4556fd390c9e7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-11-08 16:47:51 +0300

    Task 1: Babel Setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Flask i18n app """
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ Class to hold LANGUAGES attribute """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route("/")
+def hello():
+    """ flask route '/' """
+    return "<title>Welcome to Holberton</title>"
+
+#what does it mean to use Config to set Babel's default locale
+# app.config.from_object(Config)
